f29ff83bc212f4196f2e6f3a92aaa04e6256882c
Cherfez <chervasev.leeuwen@gmail.com>
2020-02-28 21:55:51 +0100

    added new components and routes to App

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import HomePage from "./pages/HomePage";
 import DiscoverMoviesPage from "./pages/DiscoverMoviesPage";
 import AboutPage from "./pages/AboutPage";
 import NavBar from "./components/NavBar";
+import MoviePage from "./pages/MoviePage";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Switch>
-        <Route path="/discover" component={DiscoverMoviesPage} />
+        <Route path="/movie/:id" component={MoviePage} />
+        <Route path="/discover/:searchtext" component={DiscoverMoviesPage} />
+        <Route path="/discover" component={DiscoverMoviesPage} />{" "}
+        {/*shows undefined movies when stating up*/}
         <Route path="/about" component={AboutPage} />
         <Route path="/" component={HomePage} />
       </Switch>
